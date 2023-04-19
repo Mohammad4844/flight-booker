a3d78dde17040086a36ac1e57f9abb190c884fe7
Mohammad <mohammadimaan484@gmail.com>
2023-04-19 12:45:35 -0400

    Updated forms for Flights
    - got rid of unnecessary Flights views for create, update &  delete
    - got rid of Flights controller methods for create, update & delete
    - changed root to flights#index, same as /flights
    - added search form for Flights in flights#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,6 @@
 class FlightsController < ApplicationController
-  before_action :set_flight, only: %i[ show edit update destroy ]
+  before_action :set_flight, only: %i[ show ]
+  before_action :set_codes
 
   # GET /flights or /flights.json
   def index
@@ -10,57 +11,16 @@ class FlightsController < ApplicationController
   def show
   end
 
-  # GET /flights/new
-  def new
-    @flight = Flight.new
-  end
-
-  # GET /flights/1/edit
-  def edit
-  end
-
-  # POST /flights or /flights.json
-  def create
-    @flight = Flight.new(flight_params)
-
-    respond_to do |format|
-      if @flight.save
-        format.html { redirect_to flight_url(@flight), notice: "Flight was successfully created." }
-        format.json { render :show, status: :created, location: @flight }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @flight.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /flights/1 or /flights/1.json
-  def update
-    respond_to do |format|
-      if @flight.update(flight_params)
-        format.html { redirect_to flight_url(@flight), notice: "Flight was successfully updated." }
-        format.json { render :show, status: :ok, location: @flight }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @flight.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /flights/1 or /flights/1.json
-  def destroy
-    @flight.destroy
-
-    respond_to do |format|
-      format.html { redirect_to flights_url, notice: "Flight was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_flight
       @flight = Flight.find(params[:id])
+    end
+
+    def set_codes
+      puts "doing this"
+      @airport_codes = Airport.all.map{ |a| [ a.code, a.id ] }
+      @airline_codes = Airline.all.map{ |a| [ a.code, a.id ] }
     end
 
     # Only allow a list of trusted parameters through.
