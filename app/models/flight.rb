2e83316244ab66f4db06f3a09a5313bbaeb6a729
Mohammad <mohammadimaan484@gmail.com>
2023-04-20 23:29:18 -0400

    Added db validations for non-user tasks
    - Added constraints on Airline & Airport codes to be All Caps strings of length 3 & 2
    - Added presence constraints to Flight start datetime & duration, and duration should be > 0
    - Added a max length for name fields to be 100 for Airport and Airline

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,9 @@ class Flight < ApplicationRecord
   belongs_to :arrive, class_name: "Airport"
   belongs_to :airline
   has_many :bookings
+
+
+  validates :start_date_time, presence: true
+  validates :duration_min, presence: true, numericality: { greater_than: 0 }
+  
 end
