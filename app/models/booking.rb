4987a17f350f70d2f9e774dce42c2f9606062774
Mohammad <mohammadimaan484@gmail.com>
2023-04-20 23:20:11 -0400

    Added db validation to user input tasks, Booking and Passenger
    - Added validations to limit passengers for each bookind to 1-4
    - Added validation to require name & email for Passanger, and limited their length to 100
    - Added formatting validation on Passenger email field

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,14 @@ class Booking < ApplicationRecord
   has_many :passengers
 
   accepts_nested_attributes_for :passengers
+
+  validate :passengers_count
+
+  def passengers_count
+    if passengers.size < 1
+      errors.add(:base, "At least one passenger is required.")
+    elsif passengers.size > 4
+      errors.add(:base, "At most four passengers are allowed.")
+    end
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
   belongs_to :booking
+
+  validates :full_name, presence: true, length: { maximum: 100 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 100 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
+
